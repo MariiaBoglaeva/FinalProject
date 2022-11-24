494547e64e678ef991eaf3ecf4160190697c563c
maribogl <maribogl@yandex.ru>
2022-11-24 12:29:23 +0300

    change program

--- Task_program/Program.cs
+++ Task_program/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк,
+// формирует массив из строк, длина которых меньше либо равно 3 символа.
+// Первоначальный массив можно задать на старте либо ввести с клавиатуры.
+
+Console.WriteLine("Введите исходный массив: ");
+string[] words = Console.ReadLine().Split(new char[] { ',',' ',';','.'});
+string[] shortWords = CreateShortWordsArray(words, 3);
+PrintArray(shortWords);
+
+string[] CreateShortWordsArray(string[] allWords, int amountChar)
+{
+    string[] shortWords = new string[allWords.Length];
+    int index = 0;
+    for (int i = 0; i < allWords.Length; i++)
+    {
+        if (allWords[i].Length <= amountChar)
+        {
+            shortWords[index] = allWords[i];
+            index++;
+        }
+    }
+    Array.Resize(ref shortWords, index);
+    return shortWords;
+}
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.WriteLine($"{arr[arr.Length - 1]}]");
+}
